8ad433b2c29a44ef5df62976016aa957e7c81c1d
Minhaz <sharifmdminhazurrahman@gmail.com>
2024-08-07 15:53:26 +0600

    styles: update content and images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+	site: "https://example.com",
+	integrations: [mdx(), sitemap(), tailwind(), react()],
+	output: "static",
+	adapter: vercel(),
 });
